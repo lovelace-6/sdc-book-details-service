49a1a3cf0d1c89debc9ea5b04ab0e65dfef17c9c
anacollado <34594754+anacollado@users.noreply.github.com>
2019-05-04 00:44:27 -0500

    change txt

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const staticPath = `${__dirname}/../public`;
 app.use('/books/:id', express.static(staticPath));
-app.get('/loaderio-47166b40a5a0075950774cb3fb9d7285', (req, res) => res.sendFile(path.resolve(`/Users/anacollado/Desktop/hr-projects/sdc-book-details-service/loaderio-key.txt`)));
+app.get('/loaderio-47166b40a5a0075950774cb3fb9d7285.txt', (req, res) => res.sendFile(path.resolve(`/Users/anacollado/Desktop/hr-projects/sdc-book-details-service/loaderio-key.txt`)));
 
 // get initial details
 app.get('/books/:id/details', (req, res) => {
